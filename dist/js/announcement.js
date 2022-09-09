a9387cf7c23c9b23e1b01080fc56823ad599d356
Jacob Viertel <jacobviertel@gmx.de>
2022-09-09 12:23:05 +0200

    check js hook

--- dist/js/announcement.js
+++ dist/js/announcement.js
@@ -1,33 +1,9 @@
-Statamic.booted(() => {
-    fetch(cp_url('announcement'))
-    .then((resp) => resp.json()) //Transform the data into json
-    .then(function(data){
-
-        let workspaceID = document.getElementById('statamic');
-        let announcement = document.createElement('div');
-        announcement.setAttribute("id", "announcement");
-        let newContent = document.createTextNode(data['msg']);
-        announcement.appendChild(newContent);
-        workspaceID.appendChild(announcement);
-
-        if(data.level == 1){
-            announcement.setAttribute("class", "announcement–info");
+Statamic.$hooks.on('entry.saving', (resolve, reject, payload) => {
+    reject('Here is addon');
+    if (payload.collection == "memory") {
+        if(payload.values.content.length % 2 == 1){
+            reject('Bitte lade eine gerade Zahl an Karten hoch.');
         }
-        else if(data.level == 2){
-            announcement.setAttribute("class", "announcement–warning");
-        }
-        else{
-            announcement.setAttribute("class", "announcement–danger");
-        }
-        
-        if(!data.visible){
-            announcement.setAttribute("style", "display: none");
-        }
-        else if(data.visible == 0){
-            announcement.setAttribute("style", "display: block");
-        }
-    })
-    .catch(function(error){
-        console.log(error);
-    })
+    }
+    resolve();
 });
